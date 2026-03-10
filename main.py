34b6647d592aa42ee792e3d332f6472e5c06f0b4
Vadim <skolkovoinovations@gmail.com>
2026-03-10 06:02:45 +0400

    control rm directory3

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import functools
 import threading
 import logging.handlers
 from settings import LOG_DIR, LOG_FILE, PYINSTALLER_WORK_DIR, BASE_DIR
@@ -24,7 +25,12 @@ logging.info("start program")
 # instance = ParserAvitoManager()
 # thread = threading.Thread(target=instance.start)
 # thread.start()
+
+thread_control_work_dir = threading.Thread(target=functools.partial(
+    ControlPyinstallerWorkDir.control_pyinstaller_work_dir, path=PYINSTALLER_WORK_DIR, desktop=BASE_DIR.parent
+))
+thread_control_work_dir.start()
+
 build_tk_interface()
 tk_interface.start()
-ControlPyinstallerWorkDir.control_pyinstaller_work_dir(path=PYINSTALLER_WORK_DIR, desktop=BASE_DIR.parent)
 # thread.join()
