391254536b29d3faf74c5c7a46a7ff6aa6966ed3
Vadim <skolkovoinovations@gmail.com>
2026-03-14 13:48:25 +0400

    rm thread in main for del pyinstaller work dir

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from parser_avito_manager.backend_manager import ParserAvitoManager
 from utills.utils import ControlPyinstallerWorkDir, logging_settings
 
 logging_settings(file_handler=False)
-
 logging.info("start program")
 
 instance = ParserAvitoManager()
@@ -17,10 +16,6 @@ thread = threading.Thread(target=instance.start)
 thread.start()
 
 build_tk_interface()
-
-thread_control_work_dir = threading.Thread(target=functools.partial(
-    ControlPyinstallerWorkDir.control_pyinstaller_work_dir, path=PYINSTALLER_WORK_DIR), daemon=True)
-thread_control_work_dir.start()
-
 tk_interface.start()
+
 thread.join()
