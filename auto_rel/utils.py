0e2934802137a900d16344baa11fc6c92011d81a
Vadim <skolkovoinovations@gmail.com>
2026-03-17 19:38:44 +0400

    bag fix

--- auto_rel/utils.py
+++ auto_rel/utils.py
@@ -4,7 +4,7 @@ from PIL import Image, ImageDraw
 def new_icon(tag, path, new_path):
     img = Image.open(path)
     draw = ImageDraw.Draw(img)
-    draw.rectangle((59, 10, 195, 50), fill='white')
-    draw.text((56, 5), tag, (50, 205, 50), font_size=40)
+    draw.rectangle((30, 5, 230, 60), fill='white')
+    draw.text((30, 3), tag, (50, 205, 50), font_size=55)
     new_img = img.resize((256, 256))
     new_img.save(new_path)
